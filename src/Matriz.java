8c071e8e5510f9a402aaadb768ed8766feb7dd32
Ian <115850010+NaiZenay@users.noreply.github.com>
2024-01-11 21:37:09 -0600

    Menu, creacion de matrices, impresion y suma

--- src/Matriz.java
+++ src/Matriz.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Matriz {
+import java.util.Scanner;
+
+public class Matriz {
+    public int[][] getMatriz() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa le tamaño de las matriz");
+        System.out.println("Filas");
+        int filas = scanner.nextInt();
+        System.out.println("Columnas");
+        int columnas = scanner.nextInt();
+        int[][] matriz = new int[filas][columnas];
+        llenarMatriz(matriz, filas, columnas);
+        return matriz;
+    }
+    public void llenarMatriz(int[][] matriz, int filas, int columnas) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.println("Ingresa el valor a guardar en la posicion [" + (i + 1) + "][" + (j + 1) + "]");
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println(imprimirMatriz(matriz, filas, columnas));
+    }
+
+    public StringBuilder imprimirMatriz(int[][] matriz, int filas, int columnas) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                stringBuilder.append("\t").append(matriz[i][j]).append(" ");
+            }
+            stringBuilder.append("\n");
+        }
+        return stringBuilder;
+    }
+
 }
+
--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Menu {
+import java.util.Scanner;
+
+public class Menu {
+    public void menu(){
+        Matriz matriz = new Matriz();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa el numero de la operacion a realizar \n" +
+                "1)Sumar matrices\n2)Restar matrices");
+        int opcionMenu = scanner.nextInt();
+
+        switch (opcionMenu) {
+            case 1:
+                boolean matrizValida;
+                int[][] matrizA;
+                int[][] matrizB;
+                do {
+                    matrizValida=true;
+                    matrizA=matriz.getMatriz() ;
+                    matrizB=matriz.getMatriz() ;
+                    if (matrizA.length != matrizB.length ){
+                        System.out.println("Las matrices no coinciden en tamaño\n" +
+                                "Segun el criterio de suma de matrices estas deben de tener el mismo numero de filas y columnas\n" +
+                                "Ingrese los valores de la matriz de nuevo:\n"
+                        );
+                        matrizValida=false;
+                    }
+                }while (!matrizValida);
+
+
+                int[][] resultado= new int[matrizA.length][matrizA.length];
+
+                for (int i = 0; i < matrizA.length; i++) {
+                    for (int j = 0; j < matrizA.length; j++) {
+                        resultado[i][j] = matrizA[i][j] + matrizB[i][j];
+                    }
+                }
+                System.out.print("Resultado de suma: \n"+matriz.imprimirMatriz(resultado,matrizB.length,matrizA.length)+"\n -FIN");
+                break;
+            default:
+                System.out.println("Programa finalizado");
+                break;
+        }
+    }
+
 }
